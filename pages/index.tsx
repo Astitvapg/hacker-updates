a786f438c8669fae8c05d577e05cbc0568620227
Astitva Singh <125200931+Astitvapg@users.noreply.github.com>
2023-03-14 18:48:42 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -125,6 +125,7 @@ const HeroSection: React.FC = () => {
                         },
                       }}
                     />
+                    />
                   }
                 
                   
